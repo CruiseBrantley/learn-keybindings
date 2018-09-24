3eeb19332491c181e2a5634dbb4140327fb0d01e
Cruise Brantley <cruise.brantley@gmail.com>
2018-09-24 15:36:53 -0500

    Bindings are now editable, also ignores shift

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
 // import Priest from "./components/classes/priest.js";
+import priestIcon from "./wowiconpack/Characters and Creatures/priest.png";
 import swp from "./wowiconpack/Spells/ShadowWordPain.png";
 import psychicScream from "./wowiconpack/Spells/PsychicScream.png";
 import shadowFiend from "./wowiconpack/Spells/Shadowfiend.png";
-import priestIcon from "./wowiconpack/Characters and Creatures/priest.png";
-
-const abilityArray = [
-  { ability: swp, bind: "3" },
-  { ability: psychicScream, bind: "c" },
-  { ability: shadowFiend, bind: "r" }
-];
+import mindFlay from "./wowiconpack/Spells/SiphonMana.png";
+import mindBlast from "./wowiconpack/Spells/UnholyFrenzy.png";
 
 class App extends Component {
   state = {
     clicked: false,
     incorrect: false,
     new: true,
-    nextAbility: abilityArray[Math.floor(Math.random() * abilityArray.length)]
+    editing: false,
+    nextAbility: {},
+    textField: "",
+    abilityArray: [
+      { ability: swp, bind: "3" },
+      { ability: psychicScream, bind: "c" },
+      { ability: shadowFiend, bind: "r" },
+      { ability: mindFlay, bind: "2"},
+      { ability: mindBlast, bind: "4"},
+    ]
   };
 
   componentDidMount() {
-    document.addEventListener("keydown", this.escFunction, false);
+    this.setState({nextAbility: this.state.abilityArray[Math.floor(Math.random() * this.state.abilityArray.length)]})
+    document.addEventListener("keydown", this.keyRead, false);
   }
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.escFunction, false);
+    document.removeEventListener("keydown", this.keyRead, false);
   }
 
-  escFunction = theKey => {
-    if (theKey.key === this.state.nextAbility.bind) this.onSubmit();
-    else this.onIncorrect();
-    console.log(theKey, "was pressed");
+  keyRead = theKey => {
+    if (this.state.editing === false && theKey.key !== "Shift") {
+      if (theKey.key === this.state.nextAbility.bind) this.onSubmit();
+      else this.onIncorrect();
+    }
   };
+
+  onChange = (e, index) => {
+    const tempArray = this.state.abilityArray;
+    tempArray[index].bind = e.target.value;
+    this.setState({[this.state.nextAbility]: tempArray})
+  }
 
   onSubmit = () => {
     this.setState({ clicked: true });
@@ -52,34 +65,55 @@ class App extends Component {
 
   getNextAbility = () => {
     this.setState({
-      nextAbility: abilityArray[Math.floor(Math.random() * abilityArray.length)]
+      nextAbility: this.state.abilityArray[Math.floor(Math.random() * this.state.abilityArray.length)]
     });
   };
 
-  getAnimationState = () => {
-    console.log(this.state);
-    if (this.state.clicked) return "current-ability-animate";
-    if (this.state.incorrect) return "current-ability-incorrect";
-    if (this.state.new) return "current-ability-init";
+  onFocus = () => {
+    this.setState({editing: true});
+  }
+
+  onBlur = () => {
+    this.setState({editing: false});
+  }
+
+  getAnimationClassName = () => {
+    if (this.state.clicked) return "current-ability-animate current-ability";
+    if (this.state.incorrect)
+      return "current-ability-incorrect current-ability";
+    if (this.state.new) return "current-ability-init current-ability";
+    else return "current-ability";
   };
 
   render() {
     // let nextAbility = this.getNextAbility();
     return (
       <div className="App">
-        <div className={"wow-class-name"}>
-          <img src={priestIcon} className={"wow-class-name-icon"} alt={""} />
-          <h1>Priest</h1>
+        <div className="name-and-ability-pane">
+          <div className={"wow-class-name"}>
+            <img src={priestIcon} className={"wow-class-name-icon"} alt={""} />
+            <h1>Priest</h1>
+          </div>
+          <div className="current-ability-container">
+            <img
+              onClick={this.onSubmit}
+              className={this.getAnimationClassName()}
+              src={this.state.nextAbility.ability}
+              alt={""}
+            />
+          </div>
+          <div className={"abilities-mapped"}>
+            {this.state.abilityArray.map((item, index) => {
+              return (
+                <div key={index} className={"individual-ability-mapped"}>
+                  <img src={item.ability} alt={""} />
+                  {/* <span>{item.bind}</span> */}
+                  <input value={this.state.abilityArray[index].bind} onChange={e => this.onChange(e, index)} onFocus={this.onFocus} onBlur={this.onBlur}/>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className="current-ability-container">
-          <img
-            onClick={this.onSubmit}
-            className={this.getAnimationState()}
-            src={this.state.nextAbility.ability}
-            alt={""}
-          />
-        </div>
-        <button onClick={this.onIncorrect}>X</button>
       </div>
     );
   }
