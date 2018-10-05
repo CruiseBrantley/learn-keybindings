af144df610002f8614a644a8015d0c695a9f985a
Cruise Brantley <cruise.brantley@gmail.com>
2018-10-05 16:45:40 -0500

    Filled priest abilities out

--- src/components/abilities&icons.js
+++ src/components/abilities&icons.js
@@ -6,6 +6,81 @@ import psychicScream from "../wowiconpack/Spells/PsychicScream.png";
 import shadowFiend from "../wowiconpack/Spells/Shadowfiend.png";
 import mindFlay from "../wowiconpack/Spells/SiphonMana.png";
 import mindBlast from "../wowiconpack/Spells/UnholyFrenzy.png";
+import smite from "../wowiconpack/Spells/HolySmite.png";
+import divineStar from "../wowiconpack/Spells/Spell_priest_divinestar.png";
+import halo from "../wowiconpack/Spells/Ability_priest_halo.png";
+import angelicFeather from "../wowiconpack/Spells/Ability_priest_angelicfeather.png";
+import desperatePrayer from "../wowiconpack/Spells/Spell_holy_testoffaith.png";
+import dispelMagic from "../wowiconpack/Spells/Spell_nature_nullifydisease.png";
+import fade from "../wowiconpack/Spells/LesserInvisibilty.png";
+import holyNova from "../wowiconpack/Spells/HolyNova.png";
+import leapOfFaith from "../wowiconpack/Spells/Priest_spell_leapoffaith_a.png";
+import levitate from "../wowiconpack/Spells/LayOnHands.png";
+import massDispel from "../wowiconpack/Spells/MassDispel.png";
+import massRes from "../wowiconpack/Spells/Achievement_guildperk_massresurrection.png";
+import mindControl from "../wowiconpack/Spells/ShadowWordDominate.png";
+import mindVision from "../wowiconpack/Spells/MindVision.png";
+import mindBender from "../wowiconpack/Spells/SoulLeech_3.png";
+import painSuppression from "../wowiconpack/Spells/PainSupression.png";
+import penance from "../wowiconpack/Spells/Penance.png";
+import powerWordBarrier from "../wowiconpack/Spells/PowerWordBarrier.png";
+import fortitude from "../wowiconpack/Spells/WordFortitude.png";
+import radiance from "../wowiconpack/Spells/Spell_priest_power_word.png";
+import powerWordShield from "../wowiconpack/Spells/PowerWordShield.png";
+import purgeTheWicked from "../wowiconpack/Abilities/FireStarter.png";
+import purify from "../wowiconpack/Spells/DISPELMAGIC.png";
+import rapture from "../wowiconpack/Spells/Rapture.png";
+import resurrection from "../wowiconpack/Spells/Resurrection.png";
+import shackleUndead from "../wowiconpack/Spells/Slow.png";
+import shadowMend from "../wowiconpack/Spells/ShadowMend.png";
+import schism from "../wowiconpack/Spells/Spell_warlock_focusshadow.png";
+import solace from "../wowiconpack/Spells/Ability_priest_flashoflight.png";
+import shiningForce from "../wowiconpack/Spells/Ability_paladin_blindinglight2.png";
+import shadowCovenant from "../wowiconpack/Spells/SummonVoidWalker.png";
+import luminousBarrier from "../wowiconpack/Spells/Spell_priest_burningwill.png";
+import evangelism from "../wowiconpack/Spells/DivineIllumination.png";
+import archangel from "../wowiconpack/Spells/Ability_priest_archangel.png";
+import darkArchangel from "../wowiconpack/Spells/Ability_priest_darkarchangel.png";
+import premonition from "../wowiconpack/Spells/Inv_helm_robe_raidpriest_k_01.png";
+import heal from "../wowiconpack/Spells/GreaterHeal.png";
+import flashHeal from "../wowiconpack/Spells/FlashHeal.png";
+import guardianSpirit from "../wowiconpack/Spells/GuardianSpirit.png";
+import holyFire from "../wowiconpack/Spells/SearingLight.png";
+import chastise from "../wowiconpack/Spells/Chastise.png";
+import bindingHeal from "../wowiconpack/Spells/BlindingHeal.png";
+import divineHymn from "../wowiconpack/Spells/DivineHymn.png";
+import holyWordSalvation from "../wowiconpack/Spells/Ability_priest_archangel.png";
+import holyWordSanctuary from "../wowiconpack/Spells/DivineProvidence.png";
+import holyWordSerenity from "../wowiconpack/Spells/Spell_holy_persuitofjustice.png";
+import prayerOfHealing from "../wowiconpack/Spells/PrayerOfHealing02.png";
+import prayerOfMending from "../wowiconpack/Spells/PrayerOfMendingtga.png";
+import renew from "../wowiconpack/Spells/Renew.png";
+import symbolOfHope from "../wowiconpack/Spells/SymbolOfHope.png";
+import circleOfHealing from "../wowiconpack/Spells/CircleOfRenewal.png";
+import apotheosis from "../wowiconpack/Spells/Ability_priest_ascension.png";
+import holyWard from "../wowiconpack/Spells/Ability_priest_soulwarding.png";
+import spiritOfTheRedeemed from "../wowiconpack/Trade/EssenceEternalLarge.png";
+import rayOfHope from "../wowiconpack/Spells/Ability_priest_rayofhope.png";
+import voidEruption from "../wowiconpack/Spells/Spell_priest_void_blast.png";
+import voidBolt from "../wowiconpack/Spells/Ability_ironmaidens_convulsiveshadows.png";
+import shadowform from "../wowiconpack/Spells/Shadowform.png";
+import purifyDisease from "../wowiconpack/Spells/Spell_holy_nullifydisease.png";
+import vampiricTouch from "../wowiconpack/Spells/Stoicism.png";
+import mindSear from "../wowiconpack/Spells/MindShear.png";
+import shadowWordDeath from "../wowiconpack/Spells/DemonicFortitude.png";
+import silence from "../wowiconpack/Spells/Ability_priest_silence.png";
+import dispersion from "../wowiconpack/Spells/Dispersion.png";
+import vampiricEmbrace from "../wowiconpack/Spells/UnsummonBuilding.png";
+import shadowWordVoid from "../wowiconpack/Spells/Spell_mage_presenceofmind.png";
+import darkVoid from "../wowiconpack/Miscellaneous/Primal_Shadow.png";
+import mindBomb from "../wowiconpack/Spells/MindBomb.png";
+import psychicHorror from "../wowiconpack/Spells/PsychicHorrors.png";
+import shadowClash from "../wowiconpack/Spells/Shadowfury.png";
+import voidTorrent from "../wowiconpack/Spells/Inv_knife_1h_artifactcthun_d_01.png";
+import darkAscension from "../wowiconpack/Spells/Achievement_boss_triumvirate_darknaaru.png";
+import surrenderToMadness from "../wowiconpack/Spells/Achievement_boss_generalvezax_01.png";
+import voidShift from "../wowiconpack/Spells/Spell_priest_voidshift.png";
+import psyfiend from "../wowiconpack/Spells/Spell_priest_psyfiend.png";
 
 import felEruption from "../wowiconpack/Spells/Ability_bossfellord_felspike.png";
 import fracture from "../wowiconpack/Spells/Ability_creature_felsunder.png";
@@ -54,54 +129,129 @@ export const PriestIcon = Priest;
 export const DemonHunterIcon = DemonHunter;
 
 export const PriestAbilities = [
-  { ability: swp, bind: "" },
-  { ability: psychicScream, bind: "" },
-  { ability: shadowFiend, bind: "" },
-  { ability: mindFlay, bind: "" },
-  { ability: mindBlast, bind: "" }
+  { ability: swp, bind: "", name: "Shadow Word: Pain" },
+  { ability: psychicScream, bind: "", name: "Psychic Scream" },
+  { ability: shadowFiend, bind: "", name: "Shadow Fiend" },
+  { ability: mindFlay, bind: "", name: "Mind Flay" },
+  { ability: mindBlast, bind: "", name: "Mind Blast" },
+  { ability: smite, bind: "", name: "Smite" },
+  { ability: divineStar, bind: "", name: "Divine Star" },
+  { ability: halo, bind: "", name: "Halo" },
+  { ability: angelicFeather, bind: "", name: "Angelic Feather" },
+  { ability: desperatePrayer, bind: "", name: "Desperate Prayer" },
+  { ability: dispelMagic, bind: "", name: "Dispel Magic" },
+  { ability: fade, bind: "", name: "Fade" },
+  { ability: holyNova, bind: "", name: "Holy Nova" },
+  { ability: leapOfFaith, bind: "", name: "Leap of Faith" },
+  { ability: levitate, bind: "", name: "Levitate" },
+  { ability: massDispel, bind: "", name: "Mass Dispel" },
+  { ability: massRes, bind: "", name: "Mass Resurrection" },
+  { ability: mindControl, bind: "", name: "Mind Control" },
+  { ability: mindVision, bind: "", name: "Mind Vision" },
+  { ability: mindBender, bind: "", name: "Mind Bender" },
+  { ability: painSuppression, bind: "", name: "Pain Suppression" },
+  { ability: penance, bind: "", name: "Penance" },
+  { ability: powerWordBarrier, bind: "", name: "Power Word: Barrier" },
+  { ability: fortitude, bind: "", name: "Power Word: Fortitude" },
+  { ability: radiance, bind: "", name: "Radiance" },
+  { ability: powerWordShield, bind: "", name: "Power Word: Shield" },
+  { ability: purgeTheWicked, bind: "", name: "Purge the Wicked" },
+  { ability: purify, bind: "", name: "Purify" },
+  { ability: rapture, bind: "", name: "Rapture" },
+  { ability: resurrection, bind: "", name: "Resurrection" },
+  { ability: shackleUndead, bind: "", name: "Shackle Undead" },
+  { ability: shadowMend, bind: "", name: "Shadow Mend" },
+  { ability: schism, bind: "", name: "Schism" },
+  { ability: solace, bind: "", name: "Power Word: Solace" },
+  { ability: shiningForce, bind: "", name: "Shining Force" },
+  { ability: shadowCovenant, bind: "", name: "Shadow Covenant" },
+  { ability: luminousBarrier, bind: "", name: "Luminous Barrier" },
+  { ability: evangelism, bind: "", name: "Evangelism" },
+  { ability: archangel, bind: "", name: "Archangel" },
+  { ability: darkArchangel, bind: "", name: "Dark Archangel" },
+  { ability: premonition, bind: "", name: "Premonition" },
+  { ability: heal, bind: "", name: "Heal" },
+  { ability: flashHeal, bind: "", name: "Flash Heal" },
+  { ability: guardianSpirit, bind: "", name: "Guardian Spirit" },
+  { ability: holyFire, bind: "", name: "Holy Fire" },
+  { ability: chastise, bind: "", name: "Chastise" },
+  { ability: bindingHeal, bind: "", name: "Binding Heal" },
+  { ability: divineHymn, bind: "", name: "Divine Hymn" },
+  { ability: holyWordSalvation, bind: "", name: "Holy Word: Salvation" },
+  { ability: holyWordSanctuary, bind: "", name: "Holy Word: Sanctuary" },
+  { ability: holyWordSerenity, bind: "", name: "Holy Word: Serenity" },
+  { ability: prayerOfHealing, bind: "", name: "Prayer of Healing" },
+  { ability: prayerOfMending, bind: "", name: "Prayer of Mending" },
+  { ability: renew, bind: "", name: "Renew" },
+  { ability: symbolOfHope, bind: "", name: "Symbol of Hope" },
+  { ability: circleOfHealing, bind: "", name: "Circle of Healing" },
+  { ability: apotheosis, bind: "", name: "Apotheosis" },
+  { ability: holyWard, bind: "", name: "Holy Ward" },
+  { ability: spiritOfTheRedeemed, bind: "", name: "Spirit of the Redeemed" },
+  { ability: rayOfHope, bind: "", name: "Ray of Hope" },
+  { ability: voidEruption, bind: "", name: "Void Eruption" },
+  { ability: voidBolt, bind: "", name: "Void Bolt" },
+  { ability: shadowform, bind: "", name: "Shadowform" },
+  { ability: purifyDisease, bind: "", name: "Purify Disease" },
+  { ability: vampiricTouch, bind: "", name: "Vampiric Touch" },
+  { ability: mindSear, bind: "", name: "Mind Sear" },
+  { ability: shadowWordDeath, bind: "", name: "Shadow Word: Death" },
+  { ability: silence, bind: "", name: "Silence" },
+  { ability: dispersion, bind: "", name: "Dispersion" },
+  { ability: vampiricEmbrace, bind: "", name: "Vampiric Embrace" },
+  { ability: shadowWordVoid, bind: "", name: "Shadow Word: Void" },
+  { ability: darkVoid, bind: "", name: "Dark Void" },
+  { ability: mindBomb, bind: "", name: "Mind Bomb" },
+  { ability: psychicHorror, bind: "", name: "Psychic Horror" },
+  { ability: shadowClash, bind: "", name: "Shadow Clash" },
+  { ability: voidTorrent, bind: "", name: "Void Torrent" },
+  { ability: darkAscension, bind: "", name: "Dark Ascension" },
+  { ability: surrenderToMadness, bind: "", name: "Surrender to Madness" },
+  { ability: voidShift, bind: "", name: "Void Shift" },
+  { ability: psyfiend, bind: "", name: "Psyfiend" }
 ];
 
 export const DemonHunterAbilities = [
-  { ability: felEruption, bind: "" },
-  { ability: fracture, bind: "" },
-  { ability: bladeDance, bind: "" },
-  { ability: blur, bind: "" },
-  { ability: chaosStrike, bind: "" },
-  { ability: disrupt, bind: "" },
-  { ability: darkness, bind: "" },
-  { ability: demonSpikes, bind: "" },
-  { ability: eyeBeam, bind: "" },
-  { ability: eyeOfLeotheras, bind: "" },
-  { ability: felblade, bind: "" },
-  { ability: felDevastation, bind: "" },
-  { ability: felrush, bind: "" },
-  { ability: fieryBrand, bind: "" },
-  { ability: shear, bind: "" },
-  { ability: illidansGrasp, bind: "" },
-  { ability: immolationAura, bind: "" },
-  { ability: imprison, bind: "" },
-  { ability: infernalStrike, bind: "" },
-  { ability: manaBreak, bind: "" },
-  { ability: metamorphasisDPS, bind: "" },
-  { ability: metamorphasisTank, bind: "" },
-  { ability: rainFromAbove, bind: "" },
-  { ability: reverseMagic, bind: "" },
-  { ability: sigilOfChains, bind: "" },
-  { ability: sigilOfInquisition, bind: "" },
-  { ability: sigilOfMisery, bind: "" },
-  { ability: sigilOfSilence, bind: "" },
-  { ability: soulCleave, bind: "" },
-  { ability: spectralSight, bind: "" },
-  { ability: throwGlaive, bind: "" },
-  { ability: torment, bind: "" },
-  { ability: vengefulRetreat, bind: "" },
-  { ability: nemesis, bind: "" },
-  { ability: felBarrage, bind: "" },
-  { ability: spiritBomb, bind: "" },
-  { ability: demonicTrample, bind: "" },
-  { ability: demonsBite, bind: "" },
-  { ability: chaosNova, bind: "" },
-  { ability: consumeMagic, bind: "" },
-  { ability: darkSlash, bind: "" },
-  { ability: netherwalk, bind: "" }
+  { ability: felEruption, bind: "", name: "Fel Eruption" },
+  { ability: fracture, bind: "", name: "Fracture" },
+  { ability: bladeDance, bind: "", name: "Blade Dance" },
+  { ability: blur, bind: "", name: "Blur" },
+  { ability: chaosStrike, bind: "", name: "Chaos Strike" },
+  { ability: disrupt, bind: "", name: "Disrupt" },
+  { ability: darkness, bind: "", name: "Darkness" },
+  { ability: demonSpikes, bind: "", name: "Demon Spikes" },
+  { ability: eyeBeam, bind: "", name: "Eye Beam" },
+  { ability: eyeOfLeotheras, bind: "", name: "Eye of Leotheras" },
+  { ability: felblade, bind: "", name: "Felblade" },
+  { ability: felDevastation, bind: "", name: "Fel Devastation" },
+  { ability: felrush, bind: "", name: "Felrush" },
+  { ability: fieryBrand, bind: "", name: "Fiery Brand" },
+  { ability: shear, bind: "", name: "Shear" },
+  { ability: illidansGrasp, bind: "", name: "Illidan's Grasp" },
+  { ability: immolationAura, bind: "", name: "Immolation Aura" },
+  { ability: imprison, bind: "", name: "Imprison" },
+  { ability: infernalStrike, bind: "", name: "Infernal Strike" },
+  { ability: manaBreak, bind: "", name: "Mana Break" },
+  { ability: metamorphasisDPS, bind: "", name: "Metamorphasis" },
+  { ability: metamorphasisTank, bind: "", name: "Metamorphasis" },
+  { ability: rainFromAbove, bind: "", name: "Rain From Above" },
+  { ability: reverseMagic, bind: "", name: "ReverseMagic" },
+  { ability: sigilOfChains, bind: "", name: "Sigil of Chains" },
+  { ability: sigilOfInquisition, bind: "", name: "Sigil of Inquisition" },
+  { ability: sigilOfMisery, bind: "", name: "Sigil of Misery" },
+  { ability: sigilOfSilence, bind: "", name: "Sigil of Silence" },
+  { ability: soulCleave, bind: "", name: "Soul Cleave" },
+  { ability: spectralSight, bind: "", name: "Spectral Sight" },
+  { ability: throwGlaive, bind: "", name: "Throw Glaive" },
+  { ability: torment, bind: "", name: "Torment" },
+  { ability: vengefulRetreat, bind: "", name: "Vengeful Retreat" },
+  { ability: nemesis, bind: "", name: "Nemesis" },
+  { ability: felBarrage, bind: "", name: "Fel Barrage" },
+  { ability: spiritBomb, bind: "", name: "Spirit Bomb" },
+  { ability: demonicTrample, bind: "", name: "Demonic Trample" },
+  { ability: demonsBite, bind: "", name: "Demon's Bite" },
+  { ability: chaosNova, bind: "", name: "Chaos Nova" },
+  { ability: consumeMagic, bind: "", name: "Consume Magic" },
+  { ability: darkSlash, bind: "", name: "Dark Slash" },
+  { ability: netherwalk, bind: "", name: "Nether Walk" }
 ];
